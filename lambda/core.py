d30966ee4afb865131d4a863ae90ed5aac134429
pranav-kural <pranavkural042@gmail.com>
2024-11-20 15:23:44 -0500

    Updated core code to remove dependency on pandas and supabase client libraries

--- lambda/core.py
+++ lambda/core.py
@@ -1,28 +1,32 @@
 import os
-import pandas as pd
+import csv
 import random
-from supabase import create_client, Client
+from datetime import datetime
 import boto3
+import requests
+import json
 
-def inject_sample_transactions():
+def inject_sample_transactions_handler(event, context):
   """
   Method to prepare sample records and insert them into Supabase table.
 
   Steps:
   1. Read data from CSV file.
   2. Select n records at random.
-  3. Add "completed_at" column with current timestamp.
-  4. Connect to Supabase client.
+  3. Add "completed_at" column with current timestamp, and "client_id" column with "test_client".
+  4. Prepare CSV data to be sent in bulk insert REST API request.
   5. Bulk insert records.
-  6. Publish job status to AWS SNS.
+     - Send bulk insert request to Supabase REST API.
+     - Publish job status to AWS SNS.
   """
-
+  print("Injecting sample transactions")
   """
   Verify all required environment variables are set
   """
   required_env_vars = [
     "SUPABASE_URL",
     "SUPABASE_KEY",
+    "SUPABASE_TABLE_NAME",
     "SNS_TOPIC_ARN",
     "S3_BUCKET_NAME",
     "S3_KEY"
@@ -30,55 +34,88 @@ def inject_sample_transactions():
   for env_var in required_env_vars:
     if os.environ.get(env_var) is None:
       raise Exception(f"{env_var} environment variable is not set")
+  print("All required environment variables are set")
     
   """
   Get sample data from AWS S3
   """
-  # Read data to pandas dataframe
+  # Read data to list of dictionaries
   s3 = boto3.client('s3')
   s3_bucket_name = os.environ.get("S3_BUCKET_NAME")
   s3_key = os.environ.get("S3_KEY")
   obj = s3.get_object(Bucket=s3_bucket_name, Key=s3_key)
-  df = pd.read_csv(obj['Body'])
+  data = obj['Body'].read().decode('utf-8').splitlines()
+  reader = csv.DictReader(data)
+  records = list(reader)
+  print(f"Sample data loaded from S3. Number of records: {len(records)}")
 
   """
   Select n records
   """
   # n can be 2 to 4 at random
   n = random.randint(2, 4)
-  df_n = df.sample(n).reset_index(drop=True)
+  selected_records = random.sample(records, n)
+  print(f"Selected {n} records at random")
 
   """
   Add "completed_at" column with current timestamp
   """
-  df_n['completed_at'] = pd.Timestamp.now()
+  current_timestamp = datetime.now().isoformat()
+  for record in selected_records:
+    record['completed_at'] = current_timestamp
+    record['client_id'] = 'test_client'
+  print("Added 'completed_at' column with current timestamp, and client_id")
+
 
   """
-  Connect to Supabase client
+  Prepare CSV data for bulk insert
   """
-  url: str = os.environ.get("SUPABASE_URL")
-  key: str = os.environ.get("SUPABASE_KEY")
-  supabase: Client = create_client(url, key)
+  # Columns
+  csv_data = "merchant,category,amount,first,last,city,state,role,currency,completed_at,client_id\n"
+  # add data for each record
+  for record in selected_records:
+    csv_data += f"\"{record['merchant']}\",{record['category']},{record['amount']},\"{record['first']}\",\"{record['last']}\",{record['city']},{record['state']},\"{record['role']}\",{record['currency']},{record['completed_at']},{record['client_id']}\n"
+  print(csv_data)
 
   """
   AWS SNS client to publish job status
   """
   sns = boto3.client('sns')
+  print("Connected to AWS SNS client")
 
   """
-  Bulk insert records
+  Send bulk insert request to Supabase through REST API
   """
-  try:
-    # Insert records
-    supabase.table("records").insert(df_n.to_dict(orient='records')).execute()
-    # Publish job status
-    sns.publish(
-      TopicArn=os.environ.get("SNS_TOPIC_ARN"),
-      Message=f"SUCCESS. Inserted {n} records. Time: {pd.Timestamp.now()}"
-    )
-  except Exception as e:
-    # Publish job status
-    sns.publish(
-      TopicArn=os.environ.get("SNS_TOPIC_ARN"),
-      Message=f"FAILED. Error: {str(e)}. Time: {pd.Timestamp.now()}"
-    )
+  # URL with table name
+  url = os.environ["SUPABASE_URL"] + "/rest/v1/" + os.environ["SUPABASE_TABLE_NAME"]
+  headers = {
+      "Content-Type": "text/csv",
+      "apikey": os.environ["SUPABASE_KEY"],
+      "Authorization": f"Bearer {os.environ['SUPABASE_KEY']}"
+  }
+  response = requests.post(url, data=csv_data, headers=headers)
+  if response.status_code == 201:
+      print("Bulk insert was successful")
+      # Publish job status
+      sns.publish(
+        TopicArn=os.environ.get("SNS_TOPIC_ARN"),
+        Message=f"SUCCESS. Inserted {n} records. Time: {current_timestamp}"
+      )
+      # lambda response
+      return {
+        'statusCode': 200,
+        'body': json.dumps('Transactions injected to records database.')
+      }
+  else:
+      print(f"Failed to send bulk insert request: {response.status_code}")
+      print(response.text)
+      # Publish job status
+      sns.publish(
+        TopicArn=os.environ.get("SNS_TOPIC_ARN"),
+        Message=f"FAILED. Error: {response.text}. Time: {current_timestamp}"
+      )
+      # lambda response
+      return {
+        'statusCode': 500,
+        'body': json.dumps(f"Failed to send bulk insert request: {response.status_code}")
+      }
